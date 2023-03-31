f87ab544fab082c06cf495b8cfd5174d1d923c06
Sora <91508322+SoraAurora@users.noreply.github.com>
2023-03-31 08:35:46 +0800

    Create SoraHub_Main_Public.lua

---
+++ SoraHub_Main_Public.lua
@@ -0,0 +1,22 @@
+--[[
+
+  /$$$$$$                                     /$$   /$$           /$$      
+ /$$__  $$                                   | $$  | $$          | $$      
+| $$  \__/  /$$$$$$   /$$$$$$  /$$$$$$       | $$  | $$ /$$   /$$| $$$$$$$ 
+|  $$$$$$  /$$__  $$ /$$__  $$|____  $$      | $$$$$$$$| $$  | $$| $$__  $$
+ \____  $$| $$  \ $$| $$  \__/ /$$$$$$$      | $$__  $$| $$  | $$| $$  \ $$
+ /$$  \ $$| $$  | $$| $$      /$$__  $$      | $$  | $$| $$  | $$| $$  | $$
+|  $$$$$$/|  $$$$$$/| $$     |  $$$$$$$      | $$  | $$|  $$$$$$/| $$$$$$$/
+ \______/  \______/ |__/      \_______/      |__/  |__/ \______/ |_______/ 
+                                                                           
+SoraHub - 31/3/2023
+
+Supported Games [4] : 
+ - Collect All Pets
+ - Monster Merge Simulator
+ - Weapon Crafting Simulator
+ - Boxing Fighters Simulator
+
+Created by Sora
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SoraAurora/SoraHub/main/SoraHub_Main.lua"))()
