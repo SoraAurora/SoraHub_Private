845d1594ef72ea5ff4d637a93fd76a8a47c52b3d
Sora <91508322+SoraAurora@users.noreply.github.com>
2023-12-25 16:28:58 +0800

    Update SoraHub_Load.lua

--- SoraHub_Load.lua
+++ SoraHub_Load.lua
@@ -8,18 +8,27 @@
 |  $$$$$$/|  $$$$$$/| $$     |  $$$$$$$      | $$  | $$|  $$$$$$/| $$$$$$$/
  \______/  \______/ |__/      \_______/      |__/  |__/ \______/ |_______/ 
                                                                            
-SoraHub - 3/4/2023
-Supported Games [5] : 
+SoraHub - 25/12/2023
+Supported Games [13] : 
  - Collect All Pets
  - Monster Merge Simulator
  - Weapon Crafting Simulator
  - Boxing Fighters Simulator [PATCHED]
  - Apocalypse Tycoon
+ - Slime Mine
+ - The Resistance Tycoon
+ - Tug of War Simulator
+ - ESYG1P
+ - MAsterBlox [PATCHED]
+ - Monster Merge Simulator
+ - Punch Simulator
+ - SPTS
+ - Universal ESP
 Supported Exploits [2] :
- - KRNL (KRNL.CA <-- Website)
- - Synapse (Possibly Buggy)
+ - KRNL (KRNL.CA <-- Website) [PATCHED]
+ - Synapse (Possibly Buggy) [PATCHED]
 Created by Sora
 Notes : 
-Testing on Synapse Currently Running...
+Synapse has joined Roblox's Anti Cheat Team and KRNL has left the scene.
 ]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SoraAurora/SoraHub_Public/main/SoraHub_Main_Obfuscated.lua"))()
